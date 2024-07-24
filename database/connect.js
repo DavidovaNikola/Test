c907761ed19f68f1366471fe21947db9c603844b
DavidovaNikola <nicholegreece@gmail.com>
2024-07-24 21:44:27 +0300

    pripojenikdatabazi

--- database/connect.js
+++ database/connect.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-const URL = "mongodb://JesteNevim:<Hfprbfy3>@ac-ogrwhxn-shard-00-00.c618paz.mongodb.net:27017,ac-ogrwhxn-shard-00-01.c618paz.mongodb.net:27017,ac-ogrwhxn-shard-00-02.c618paz.mongodb.net:27017/?ssl=true&replicaSet=atlas-noacqi-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const URL = "mongodb://JesteNevim:Hfprbfy3@ac-ogrwhxn-shard-00-00.c618paz.mongodb.net:27017,ac-ogrwhxn-shard-00-01.c618paz.mongodb.net:27017,ac-ogrwhxn-shard-00-02.c618paz.mongodb.net:27017/?ssl=true&replicaSet=atlas-noacqi-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
 
 class dbConnect {
     static async connect() {
         try {
             await mongoose.connect(URL, {
-                useNewUrlParser: true,
                 useUnifiedTopology: true,
-                useFindAndModify: true,
-                useCreateIndex: true
             });
             console.log("Successfully connected to the database");
         } catch (err) {
